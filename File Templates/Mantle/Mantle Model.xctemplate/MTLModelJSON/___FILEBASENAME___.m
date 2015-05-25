ca62f6bed53265c2ed715ccca39c2b101316c1a7
Ashton Williams <AJWilliams@odecee.com.au>
2015-05-25 22:37:32 +1000

    init

---
+++ File Templates/Mantle/Mantle Model.xctemplate/MTLModelJSON/___FILEBASENAME___.m
@@ -0,0 +1,31 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionaryValue error:(NSError **)error {
+    self = [super initWithDictionary:dictionaryValue error:error];
+    if (self == nil) return nil;
+
+    // Store a value that needs to be determined locally upon initialization.
+    //_retrievedAt = [NSDate date];
+
+    return self;
+}
+
+#pragma mark - MTLJSONSerialization
+
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
+    return @{
+
+    };
+}
+
+@end
